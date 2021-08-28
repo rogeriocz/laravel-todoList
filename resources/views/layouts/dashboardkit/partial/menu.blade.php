720c323c2726b77a6c8a6e8631d1662d1b30cdec
rogeriocz <rogeriocz@gmail.com>
2021-08-28 16:40:29 -0500

    se eliminan parte del diseño del menu2

--- resources/views/layouts/dashboardkit/partial/menu.blade.php
+++ resources/views/layouts/dashboardkit/partial/menu.blade.php
@@ -4,8 +4,8 @@
         <div class="m-header">
             <a href="index.html" class="b-brand">
                 <!-- ========   change your logo hear   ============ -->
-                <img src="{{asset('dashboardKit/assets/images/logo.svg')}}" alt="" class="logo logo-lg">
-                <img src="{{asset('dashboardKit/assets/images/logo-sm.svg')}}" alt="" class="logo logo-sm">
+                <img src="{{ asset('dashboardKit/assets/images/logo.svg') }}" alt="" class="logo logo-lg">
+                <img src="{{ asset('dashboardKit/assets/images/logo-sm.svg') }}" alt="" class="logo logo-sm">
             </a>
         </div>
         <div class="navbar-content">
@@ -14,11 +14,16 @@
                     <label>Navigation</label>
                 </li>
                 <li class="pc-item">
-                    <a href="index.html" class="pc-link "><span class="pc-micon"><i class="material-icons-two-tone">home</i></span><span class="pc-mtext">Dashboard</span></a>
+                    <a href="index.html" class="pc-link "><span class="pc-micon"><i
+                                class="material-icons-two-tone">home</i></span><span
+                            class="pc-mtext">Dashboard</span></a>
                 </li>
 
                 <li class="pc-item pc-hasmenu">
-                    <a href="#!" class="pc-link "><span class="pc-micon"><i class="material-icons-two-tone">business_center</i></span><span class="pc-mtext">Basic</span><span class="pc-arrow"><i data-feather="chevron-right"></i></span></a>
+                    <a href="#!" class="pc-link "><span class="pc-micon"><i
+                                class="material-icons-two-tone">business_center</i></span><span
+                            class="pc-mtext">Basic</span><span class="pc-arrow"><i
+                                data-feather="chevron-right"></i></span></a>
                     <ul class="pc-submenu">
                         <li class="pc-item"><a class="pc-link" href="bc_alert.html">Todo List JS</a></li>
                     </ul>
