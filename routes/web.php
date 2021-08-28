ae4e69a425b52b5963f9b9a2439a26df80ea05a0
rogeriocz <rogeriocz@gmail.com>
2021-08-28 16:38:35 -0500

    se eliminan parte del diseño del menu

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TodolistController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,10 +15,11 @@ Route::get('/dashboard', function () {
 require __DIR__.'/auth.php';
 
 //CRUD DE PRACTICA CON FETCH JAVASCRIPT
-
+Route::group(['middleware' => ['auth']], function () {
 Route::get('todolist/template', [TodolistController::class, 'template'])->name('template');
 Route::get('todolist/list', [TodolistController::class, 'list'])->name('list');
 Route::get('todolist', [TodolistController::class, 'index'])->name('todolist.index');
 Route::post('todolist/newtodo', [TodolistController::class, 'store'])->name('todolist.store');
 Route::put('todolist/updatetodolist/{id}', [TodolistController::class, 'update'])->name('todolist.update');
 Route::delete('todolist/deletetodolist/{id}', [TodolistController::class, 'delete'])->name('todolist.delete');
+});
