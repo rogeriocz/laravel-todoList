cc84d8ccf01e84c3e861c2c9519e79142642aad9
rogeriocz <rogeriocz@gmail.com>
2021-08-29 09:27:44 -0500

    se configura vue y se arreglan todos los errores que marcaba vue al compilar con webpack

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,12 @@
 require('./bootstrap');
 
 require('alpinejs');
+
+import Vue from 'vue';
+window.Vue = require('vue');
+
+Vue.component('vue-example-name', require('./components/VueExample.vue').default);
+
+const app = new Vue({
+    el: '#app',
+});
---
+++ resources/js/components/VueExample.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">Header Example Vue 3</div>
+          <div class="card-body">Hello from my first vue component</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log('Hi here');
+  }
+}
+</script>
